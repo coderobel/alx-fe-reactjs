f827a6d401f87b38e65df7d7d65df44e8ef5c833
coderobel <robel.ugr-4552-16@aau.edu.et>
2025-08-26 12:33:14 +0300

    created UserProfile.jsx file

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -6,6 +6,7 @@ import WelcomeMessage from './components/WelcomeMessage'
 import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
+import UserProfile from './components/UserProfile'
 function App() {
   const [count, setCount] = useState(0)
 
@@ -24,6 +25,7 @@ function App() {
       <Header />
       <MainContent />
       <Footer />
+      <UserProfile name="Robel" age="19" bio="Loves football" />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
