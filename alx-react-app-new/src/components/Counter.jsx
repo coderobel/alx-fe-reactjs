2c5ac785f7ab1912527141a8f51845efba18fa8e
coderobel <robel.ugr-4552-16@aau.edu.et>
2025-09-04 15:56:41 +0300

    updated file

--- alx-react-app-new/src/components/Counter.jsx
+++ alx-react-app-new/src/components/Counter.jsx
@@ -1,6 +1,13 @@
 import React, { useState } from 'react';
 export default function Counter(){
+    let [count, setCount] = useState(0);
     return(
-
+        <div>
+            <h1>Counter</h1>
+            <p>You have clicked {count} times</p>
+            <button onClick= {() =>  setCount(prev + 1)}>
+                Click me
+            </button>
+        </div>
     );
 }
