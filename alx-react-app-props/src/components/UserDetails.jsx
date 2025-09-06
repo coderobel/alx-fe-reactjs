47d500721b33301d822ee11c1946d7c27f2d6bc3
coderobel <robel.ugr-4552-16@aau.edu.et>
2025-09-06 18:38:56 +0300

    made updates

--- alx-react-app-props/src/components/UserDetails.jsx
+++ alx-react-app-props/src/components/UserDetails.jsx
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import UserContext from "../assets/UserContext";
+import UserContext from "./UserContext";
 
 function UserDetails() {
     const data = useContext(UserContext);
