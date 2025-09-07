aaf0d47ea55eb15ffb548a5e1d8244a4299e4e21
coderobel <robel.ugr-4552-16@aau.edu.et>
2025-09-07 16:45:51 +0300

    made updates

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -3,18 +3,18 @@ import { Link } from "react-router-dom"
 export default function Navbar(){
     return (
         <>
-          <div>
-            <div style={{margin: '20px'}}>
-                <Link to="/" style={{color: 'black', fontSize: '20px'}}>Home</Link>
+          <div style={{backgroundColor: 'lightgrey', display: 'flex', flexDirection: 'row', height: '590px', width: '1100px', justifyContent: "flex-start"}}>
+            <div style={{ marginTop: '20px', marginLeft: '50px'}}>
+                <Link to="/" style={{boxSizing: "border-box", color: 'black', fontSize: '20px'}}>Home</Link>
             </div>
-            <div style={{margin: '20px'}}>
+            <div style={{boxSizing: "border-box", marginTop: '20px', marginLeft: '50px'}}>
                 <Link to="/about" style={{color: 'black', fontSize: '20px'}}>About</Link>
             </div>
-            <div style={{margin: '20px'}}>
+            <div style={{boxSizing: "border-box", marginTop: '20px', marginLeft: '50px'}}>
                 <Link to="/services" style={{color: 'black', fontSize: '20px'}}>Serivces</Link>
             </div>
-            <div style={{margin: '20px', fontSize: '20px'}}>
-                <Link to="/contact" style={{color: 'black'}}>Contact</Link>
+            <div style={{marginTop: '20px', marginLeft: '50px'}}>
+                <Link to="/contact" style={{color: 'black', fontSize: '20px'}}>Contact</Link>
             </div>
           </div>
         </>
