1c5108508a79bcfae52d7c2f28d9023ba30a132a
coderobel <robel.ugr-4552-16@aau.edu.et>
2025-09-07 16:22:57 +0300

    made updates

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -2,11 +2,21 @@ import { Link } from "react-router-dom"
 
 export default function Navbar(){
     return (
-        <div>
-            <Link to="/">Home</Link>
-            <Link to="/about">About</Link>
-            <Link to="/services">Serivces</Link>
-            <Link to="/contact">Contact</Link>
-        </div>
+        <>
+          <div>
+            <div style={{margin: '20px'}}>
+                <Link to="/" style={{color: 'black', fontSize: '20px'}}>Home</Link>
+            </div>
+            <div style={{margin: '20px'}}>
+                <Link to="/about" style={{color: 'black', fontSize: '20px'}}>About</Link>
+            </div>
+            <div style={{margin: '20px'}}>
+                <Link to="/services" style={{color: 'black', fontSize: '20px'}}>Serivces</Link>
+            </div>
+            <div style={{margin: '20px', fontSize: '20px'}}>
+                <Link to="/contact" style={{color: 'black'}}>Contact</Link>
+            </div>
+          </div>
+        </>
     );
 }
