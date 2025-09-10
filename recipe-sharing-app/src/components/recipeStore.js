425b3c98584fbb8e8953a33eed726809fae8ac65
coderobel <robel.ugr-4552-16@aau.edu.et>
2025-09-10 11:12:07 +0300

    made updates

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -3,7 +3,9 @@ import  { create } from 'zustand';
 const useRecipeStore = create(set => ({
     recipes: [],
     addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe]})),
-    setRecipes: (recipes) => set({recipes})
+    setRecipes: (recipes) => set({recipes}),
+    deleteRecipe: (index) => set(state => ({recipes: state.recipes.map((recipe,i) => i === index?'':recipe)})),
+    updateRecipe: (index, updatedRecipe) => set(state => ({recipes: state.recipes.map((recipe,i) => i === index?updatedRecipe:recipe)})),
 }));
 
 export default useRecipeStore;
