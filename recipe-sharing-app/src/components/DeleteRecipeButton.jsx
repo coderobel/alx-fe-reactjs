8a2c9036ba49918e0ec44a64ab1b23e8f2f9e333
coderobel <robel.ugr-4552-16@aau.edu.et>
2025-09-14 12:14:04 +0300

    made changes

--- recipe-sharing-app/src/components/DeleteRecipeButton.jsx
+++ recipe-sharing-app/src/components/DeleteRecipeButton.jsx
@@ -1,13 +1,15 @@
 import useRecipeStore from "./recipeStore";
 import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 const DeleteRecipe = ({recipeId}) => {
     const deleteRecipe = useRecipeStore(state => state.DeleteRecipe);
     const recipeToDelete = useRecipeStore(state => state.recipes.find(recipe =>recipe.id === recipeId));
-    
+    const navigate = useNavigate();
         const handleDelete = (event) => {
             event.preventDefault();
             if(recipeToDelete){
                 deleteRecipe(recipeId);
+                navigate('/');
             }
         };
         if(!recipeToDelete) {
