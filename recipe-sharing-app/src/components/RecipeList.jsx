17a5c2de08a10e2c854d96c10a3aa2d242d99103
coderobel <robel.ugr-4552-16@aau.edu.et>
2025-09-14 15:25:28 +0300

    made updates

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,7 +1,7 @@
- import  useRecipeStore  from './recipeStore';
-
+import  useRecipeStore  from './recipeStore';
+import { Link } from 'react-router-dom';
   const RecipeList = () => {
-    const recipes = useRecipeStore(state => state.recipes);
+    const recipes = useRecipeStore(state => state.filteredRecipes);
 
     return (
       <div>
