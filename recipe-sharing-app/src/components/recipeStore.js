0cbfce1bf7dfe25111a2a99e57a716e9c74ea647
coderobel <robel.ugr-4552-16@aau.edu.et>
2025-09-14 15:04:43 +0300

    made updates

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -6,6 +6,12 @@ const useRecipeStore = create(set => ({
     setRecipes: (recipes) => set({recipes}),
     deleteRecipe: (index) => set(state => ({recipes: state.recipes.map((recipe,i) => i === index?'':recipe)})),
     updateRecipe: (index, updatedRecipe) => set(state => ({recipes: state.recipes.map((recipe,i) => i === index?updatedRecipe:recipe)})),
+    searchTerm: '',
+    setSearchTerm: (term) => set({ searchTerm: term }),
+    filteredRecipes: [],
+    filterRecipes: () => set(state => ({
+      filteredRecipes: state.recipes.filter(recipe => recipe.title.toLowerCase().includes(state.searchTerm.toLowerCase()))
+    })),
 }));
 
 export default useRecipeStore;
