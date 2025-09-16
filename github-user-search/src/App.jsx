fc9ed24b1cc4c96ecd1945a96244ed103cbfb041
coderobel <robel.ugr-4552-16@aau.edu.et>
2025-09-16 18:47:21 +0300

    made updates

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,9 +1,10 @@
 import Search from "./components/Search";
 
 export default function App() {
-  <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
       <h1 className="text-4xl font-bold text-blue-600 mb-6">GitHub User Search</h1>
       <Search /> 
   </div>
+  );
 };
-
--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -62,4 +62,5 @@ function Search (){
     </div>
   );
 }
+
 export default Search;
