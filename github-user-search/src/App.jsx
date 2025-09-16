1ac751a66db420942d8cfb25f4f0e5bc1d48eafb
coderobel <robel.ugr-4552-16@aau.edu.et>
2025-09-16 13:17:04 +0300

    made updates

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -3,7 +3,7 @@ import Search from "./components/Search";
 export default function App() {
   <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
       <h1 className="text-4xl font-bold text-blue-600 mb-6">GitHub User Search</h1>
-      <Search />
+      <Search /> 
   </div>
 };
 
