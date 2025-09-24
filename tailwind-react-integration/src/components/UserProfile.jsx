35bc5e9c378f0bbb564821d333549006c6808c4a
coderobel <robel.ugr-4552-16@aau.edu.et>
2025-09-24 13:41:04 +0300

    made updates

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,6 +1,6 @@
 function UserProfile() {
   return (
-    <div className="user-profile" class="bg-gray-100 md:p-8 md:max-w-xs sm:p-4 sm:max-w-sm mx-auto my-20 rounded-lg shadow-lg">
+    <div className="user-profile" class="bg-gray-100 md:p-8 sm:max-w-xs sm:p-4 md:max-w-sm mx-auto my-20 rounded-lg shadow-lg">
       <img src="https://via.placeholder.com/150" alt="User" class="rounded-full w-36 h-36 mx-auto"/>
       <h1 class="text-xl text-blue-800 my-4">John Doe</h1>
       <p class="text-gray-600 text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
