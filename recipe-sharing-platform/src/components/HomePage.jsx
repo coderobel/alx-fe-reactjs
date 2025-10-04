6cbf5f73c6fa30f63d9f63a4852b14ac32de7b79
coderobel <robel.ugr-4552-16@aau.edu.et>
2025-10-04 18:19:34 +0300

    made updates

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -8,11 +8,11 @@ export default function HomePage() {
         .then((jsonData) => setData(jsonData));
     }, []);
     return (
-        <div className="p-8 hover:bg text-blue-800">
+        <div className="p-8">
             <h1 className="text-3xl font-bold mb-6">Recipes</h1>
             <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
                 {data.map((member) => (
-                    <div key={member.id} className="bg-white p-6 rounded-lg shadow">
+                    <div key={member.id} className="bg-white hover:bg-amber-100 p-6 rounded-lg shadow">
                         <h2 className="text-lg font-semibold">{member.title}</h2>
                         <p className="text-gray-500">{member.summary}</p>
                     </div>
