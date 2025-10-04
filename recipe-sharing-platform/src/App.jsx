3a8cc4d5d384d0909d91c47e69e97a490862fbc6
coderobel <robel.ugr-4552-16@aau.edu.et>
2025-10-04 20:02:54 +0300

    made updates

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -4,16 +4,14 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import HomePage from './components/HomePage'
 import RecipeDetail from './components/RecipeDetail'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes, Router } from 'react-router-dom'
 
 function App() {
   return (
-    <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/recipe/:id" element={<RecipeDetail />} />
       </Routes>
-    </Router>
   );
 }
 
--- recipe-sharing-platform/src/components/RecipeDetail.jsx
+++ recipe-sharing-platform/src/components/RecipeDetail.jsx
@@ -1,3 +1,32 @@
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+
 export default function RecipeDetail() {
-    
+    const { id } = useParams();
+    const [recipe, setRecipe] = useState(null);
+
+    useEffect(() => {
+        fetch("/data.json")
+        .then((res) => res.json())
+        .then((data) => {
+            const foundRecipe = data.find((item) => item.id === Number(id));
+            setRecipe(foundRecipe);
+        })
+    }, [id]);
+
+    if (!recipe) {
+    return (
+      <div className="p-8">
+        <p>Loading recipe details...</p>
+      </div>
+    );
+    }
+    return(
+        <div className="p-8">
+            <h1 className="text-3xl font-bold">Recipe Details</h1>
+            <h1 className="text-3xl font-bold mb-4">{recipe.title}</h1>
+            <p className="text-gray-600 mb-4">{recipe.summary}</p>
+            <p className="text-gray-800">More details about this recipe coming soon...</p>
+        </div>
+    );
 }
