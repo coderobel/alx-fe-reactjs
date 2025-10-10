0de7292694c2a2ccae145b9474f38455891d6dde
coderobel <robel.ugr-4552-16@aau.edu.et>
2025-10-10 19:52:08 +0300

    made updates

--- form-handling-react/src/components/formikForm.js
+++ form-handling-react/src/components/formikForm.js
@@ -9,7 +9,6 @@ const validationSchema = Yup.object({
         .email("Invalid email format")
         .required("Email is required"),
     password: Yup.string()
-        .min(6, "Password must be at least 6 characters long")
         .required("Password is required"),
 });
 
