e3788700e2119f2315e5f3971073779954cc7073
coderobel <robel.ugr-4552-16@aau.edu.et>
2025-10-11 22:18:12 +0300

    made updates

--- react-query-demo/src/components/PostsComponent.jsx
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -5,12 +5,12 @@ const fetchData = async () => {
     return res.json();
 };
 export default function PostsComponent () {
-    const { data, error, isLoading } = useQuery('fetchData', fetchData);
+    const { data, isError, isLoading } = useQuery('fetchData', fetchData);
 
     // Handle loading state
     if (isLoading) return <div>Loading...</div>;
     // Handle error state
-    if (error) return <div>Error loading data</div>;
+    if (isError) return <div>Error loading data</div>;
 
     // Render the fetched data
     return (
